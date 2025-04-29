c04029b88e6b051532aea3a1d86667ce788c745f
z3ntl3 <z3ntl3discord@gmail.com>
2025-04-29 09:08:41 +0200

    04/29/2025 09:08:40

--- bot/client/bot.go
+++ bot/client/bot.go
@@ -1,15 +1,19 @@
 package client
 
 import (
-	// todo make own lb impl because the below one is not accomodating our needs
-
+	"errors"
+	"fmt"
 	"io"
 	"log"
+	"net/url"
 	"os"
 	"path"
 	"strings"
 	"z3ntl3/storm/globals"
 	lb "z3ntl3/storm/lb/robin"
+
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpproxy"
 )
 
 type Bot struct {
@@ -20,6 +24,12 @@ type resources struct {
 	Proxies *lb.RoundRobin[string]
 	Accepts *lb.RoundRobin[string]
 	Headers *lb.RoundRobin[string]
+}
+
+type MessageContext struct {
+	Msg  string
+	Err  error
+	Kill bool
 }
 
 func New() *Bot {
@@ -66,7 +76,51 @@ func New() *Bot {
 // The body is thread-safe
 //
 // Arguments should be passed by [Bot]'s Next method on the fields as they comfort [lb.RoundRobin]
-func (c *Bot) Stress(proxy, headers, accepts string) {
-	// client := fasthttp.Client{}
-	// req := fasthttp.AcquireRequest()
+func (c *Bot) Stress(proxy string, th_id uint64, pool_msg chan MessageContext) {
+	var client fasthttp.Client
+
+	proxyURI, err := url.Parse(proxy)
+	if err != nil {
+		pool_msg <- MessageContext{
+			Err: err,
+		}
+	}
+
+	switch proxyURI.Scheme {
+	case "http", "https":
+		client.Dial = fasthttpproxy.FasthttpHTTPDialerDualStack(proxy)
+	case "socks5":
+		client.Dial = fasthttpproxy.FasthttpSocksDialerDualStack(proxy)
+	default:
+		pool_msg <- MessageContext{
+			Err:  errors.New("unsupported proxy protocol: socks4, use socks5 or http/https instead"),
+			Kill: true,
+		}
+	}
+
+	req := fasthttp.AcquireRequest()
+	for header := range c.Rsrc.Headers.Iter() {
+		if !strings.Contains(*header, " ") {
+			continue
+		}
+		h := strings.Split(*header, " ")
+		req.Header.Set(h[0], h[1])
+	}
+
+	for accept := range c.Rsrc.Accepts.Iter() {
+		req.Header.Set("Accept", *accept)
+	}
+
+	// do not observe response as to save memory
+	err = client.Do(req, nil)
+	if err != nil {
+		pool_msg <- MessageContext{
+			Msg: fmt.Sprintf("thread [%d] sending payload responded with: %s", th_id, err),
+		}
+		return
+	}
+
+	pool_msg <- MessageContext{
+		Msg: fmt.Sprintf("thread [%d] payload successfully sent: %s", th_id, req.Header.String()),
+	}
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module z3ntl3/storm
 
 go 1.23.3
 
-require github.com/spf13/cobra v1.9.1
+require (
+	github.com/spf13/cobra v1.9.1
+	github.com/valyala/fasthttp v1.60.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
@@ -10,5 +13,6 @@ require (
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.60.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 )
